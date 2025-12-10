128d9722a8f35f6a87ec5cfae46642fb23ccb7e8
husseinom <o.samy1110@gmail.com>
2025-12-10 23:23:00 +0100

    Updating pom.xml jdbc version

---
+++ src/main/java/com/syncstudy/UI/AppUI.java
@@ -0,0 +1,12 @@
+package com.syncstudy.UI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("SyncStudy");
+        stage.show();
+    }
+}
